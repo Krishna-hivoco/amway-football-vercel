8e8248b786af613432d47b65a0cfd2b66412bf54
Krishna-hivoco <krishna@hivoco.com>
2025-09-12 02:21:26 +0530

    first commit

--- src/elements/Button.tsx
+++ src/elements/Button.tsx
@@ -9,7 +9,7 @@ function Button({ title, onClick }: button) {
   return (
     <button
       onClick={onClick}
-      className={`rounded-full  p-2 w-full bg-white text-black quantico-bold `}
+      className={`rounded-full  p-3 w-full bg-white text-black quantico-bold `}
     >
       {title}
     </button>
